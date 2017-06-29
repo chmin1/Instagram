708870994b9df31203e787faa14fae270c3d4bc8
Chavane Minto <chevy@fb.com>
2017-06-28 17:33:38 -0700

    Compose View Controller now takes and saves images using Parse. Those images are queried and sent to the Home Feed View Controller. Home Feed can show the last 20 post added to Instagram

---
+++ instagram/InstaCell.swift
@@ -0,0 +1,24 @@
+//
+//  InstaCell.swift
+//  instagram
+//
+//  Created by Chavane Minto on 6/28/17.
+//  Copyright © 2017 Chavane Minto. All rights reserved.
+//
+
+import UIKit
+
+class InstaCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ instagram/Post.swift
@@ -0,0 +1,13 @@
+//
+//  Post.swift
+//  instagram
+//
+//  Created by Chavane Minto on 6/28/17.
+//  Copyright © 2017 Chavane Minto. All rights reserved.
+//
+
+import UIKit
+
+class Post: NSObject {
+
+}
